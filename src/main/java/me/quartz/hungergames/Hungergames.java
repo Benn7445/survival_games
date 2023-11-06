f9e00d22aa93af3f201d6045e7d3b7432297e67c
Ben Verbraecken <ben.verbraecken@gmail.com>
2023-11-06 21:25:42 +0100

    Hunger Games

---
+++ src/main/java/me/quartz/hungergames/Hungergames.java
@@ -0,0 +1,15 @@
+package me.quartz.hungergames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hungergames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
